bb1fedda852fdec83e866ae919d45e42daa3c488
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-09-07 16:58:56 +0200

    Authentication and Authorization

--- services/server.js
+++ services/server.js
@@ -5,8 +5,25 @@ const dotenv = require("dotenv");
 const postsRoutes = require("./posts");
 const experienceRoute = require("./experience");
 const commentRoutes = require("./comments");
-
+const userRouter = require("./auth/user")
+const cookieParser = require("cookie-parser");
 const server = express();
+const passport = require("passport")
+server.use(passport.initialize())
+
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+server.use(cookieParser());
+
 const listEndpoints = require("express-list-endpoints");
 const profilesRouter = require("./profiles/index");
 const {
@@ -17,7 +34,7 @@ const {
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3005;
 server.use(express.json());
 
 server.use(cors());
@@ -25,6 +42,7 @@ server.use("/posts", postsRoutes);
 server.use("/profile", experienceRoute);
 server.use("/profile", profilesRouter);
 server.use("/comments", commentRoutes);
+server.use("/user",userRouter);
 
 server.use(badRequestHandler);
 server.use(notFoundHandler);
@@ -43,6 +61,7 @@ mongoose
       console.log(`working on port ${port}`);
     })
   );
-mongoose.connection.on("connected", () => {
+/* mongoose.connection.on("connected", () => {
   console.log("connected to atlas");
 });
+ */
